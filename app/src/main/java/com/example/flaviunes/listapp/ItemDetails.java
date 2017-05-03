0227dbf876cc42b852a4dc22e690197b769164df
Flaviu Nes <flaviunes@gmail.com>
2017-05-03 16:10:21 +0300

    First commit

---
+++ app/src/main/java/com/example/flaviunes/listapp/ItemDetails.java
@@ -0,0 +1,32 @@
+package com.example.flaviunes.listapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ItemDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+
+        Intent intent = getIntent();
+    }
+
+    public void saveNote(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Item saved succesfully!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void cancel(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/flaviunes/listapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.flaviunes.listapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addNote(View view) {
+        Intent intent = new Intent(this, ItemDetails.class);
+        startActivity(intent);
+    }
+
+
+}
