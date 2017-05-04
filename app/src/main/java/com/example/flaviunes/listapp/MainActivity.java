1333afc225b4889a67eefcdc922f3a81d0b927b5
Flaviu Nes <flaviunes@gmail.com>
2017-05-04 12:32:06 +0300

    Created a List in MainActivity to display

--- app/src/main/java/com/example/flaviunes/listapp/MainActivity.java
+++ app/src/main/java/com/example/flaviunes/listapp/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.flaviunes.listapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private String[] myDataset = {"Note 1", "Note 2", "Note 3", "Note 4", "Note 5"};
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     public void addNote(View view) {
